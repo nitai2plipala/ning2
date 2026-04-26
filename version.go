0b4ae56df29c61780db50bf3275e32591a0c273c
nitai2plipala <761374375@qq.com>
2026-04-26 13:08:07 +0800

    Add version constants

---
+++ version.go
@@ -0,0 +1,15 @@
+// Package ning2 provides a lightweight Go web framework and crawler toolkit.
+//
+// Version: 1.0.0
+package ning2
+
+// Version constants
+const (
+	Version = "1.0.0"
+	// Major version
+	Major = 1
+	// Minor version
+	Minor = 0
+	// Patch version
+	Patch = 0
+)
